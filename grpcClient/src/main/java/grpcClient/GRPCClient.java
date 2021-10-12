0d7e401f440ca8779138645486d5d54e008df8c0
Abdülkerim AKSAK <abdulkerim.aksak@outlook.com>
2021-10-12 16:45:00 +0300

    Update GRPCClient.java

--- grpcClient/src/main/java/grpcClient/GRPCClient.java
+++ grpcClient/src/main/java/grpcClient/GRPCClient.java
@@ -1,5 +1,12 @@
 package grpcClient;
 
+import com.kerim.grpc.userGrpc;
+import java.net.ResponseCache;
+
+import com.kerim.grpc.User.APIResponse;
+import com.kerim.grpc.User.LoginRequest;
+import com.kerim.grpc.userGrpc.userBlockingStub;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -9,6 +16,12 @@ public class GRPCClient {
 		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090).usePlaintext().build();
 		
 		// stubs - generate from proto file
+		userBlockingStub userStub = userGrpc.newBlockingStub(channel);
+		
+		LoginRequest loginRequest = LoginRequest.newBuilder().setUsername("Kerim").setPassword("Kerim").build();
+		APIResponse response = userStub.login(loginRequest);
+		
+		System.out.println("Response message..:" + response.getResponsemessage() + " Response code..:" +response.getResponseCode());
 	}
 
 }
